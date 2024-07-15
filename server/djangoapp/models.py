c92126b091f0bd502615d6adcbae44545a348a12
Krzysztof <krzysztof.swiatek@student.uj.edu.pl>
2024-07-15 15:31:06 +0200

    Lint fixes

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -17,6 +16,7 @@ class CarMake(models.Model):
     def __str__(self):
         return self.name
 
+
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many
 # Car Models, using ForeignKey field)
@@ -27,7 +27,8 @@ class CarMake(models.Model):
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    # Many-to-One relationship
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -42,10 +43,10 @@ class CarModel(models.Model):
 
     type = models.CharField(max_length=16, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
-        validators=[
-            MaxValueValidator(2023),
-            MinValueValidator(2015)
-        ])
+                               validators=[
+                                 MaxValueValidator(2023),
+                                 MinValueValidator(2015)
+                               ])
 
     def __str__(self):
         return self.name
